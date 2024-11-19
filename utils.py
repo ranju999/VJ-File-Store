77b1352311c0ae8848411423dc68b344e3a76bdf
ranju999 <ranjuvishwakarma.50@gmail.com>
2024-11-19 20:39:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,6 +8,28 @@ logger.setLevel(logging.INFO)
 TOKENS = {}
 VERIFIED = {}
 
+
+def get_file_id(message: "Message") -> Any:
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )    
+    if message.media:
+        for attr in media_types:
+            media = getattr(message, attr, None)
+            if media:
+                setattr(media, "message_type", attr)
+                return media
+
+def get_hash(media_msg: Message) -> str:
+    media = get_file_id(media_msg)
+    return getattr(media, "file_unique_id", "")[:6]
 async def get_verify_shorted_link(link):
     if SHORTLINK_URL == "api.shareus.io":
         url = f'https://{SHORTLINK_URL}/easy_api'
